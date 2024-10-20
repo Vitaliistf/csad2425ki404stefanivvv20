d9a1950cfff98caf2b64ff3bcfd9924195000c91
Vitalii Stefaniv <deosplayt@gmail.com>
2024-10-20 19:35:03 +0300

    feat: added server-side code RPS-3

---
+++ server/server.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String receivedMessage = Serial.readStringUntil('\n');
+    
+    String modifiedMessage = receivedMessage + " from Arduino";
+
+    Serial.println(modifiedMessage);
+  }
+}
